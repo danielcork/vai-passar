cbb23722b4e5647401759f487aea155634ab5bb6
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-07-28 14:44:18 -0300

    tirando php

--- includes/styles.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/share.css" />
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-<link rel="stylesheet" type="text/css" href="css/main.css" />
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="css/font-awesome-animation.min.css">
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="css/previsor.css" />
-<link rel="stylesheet" href="css/seletor.css">
